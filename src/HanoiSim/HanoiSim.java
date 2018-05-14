c7cde2bba20c835b110662a69ae0ddd6d2b22200
Karan Joshi <karanjoshi31@live.com>
2018-05-13 20:34:51 -0500

    added basic menu structure

--- src/HanoiSim/HanoiSim.java
+++ src/HanoiSim/HanoiSim.java
@@ -1,7 +1,9 @@
 package HanoiSim;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class HanoiSim {
     private int diskCount;
@@ -76,18 +78,89 @@ public class HanoiSim {
 
     }
 
+    void resetSimulation(HanoiTower a, HanoiTower b,  HanoiTower c){
+        a.diskSet = (ArrayList<HanoiDisk>) c.diskSet.clone();
+        c.diskSet.clear();
+        b.diskSet.clear();
+    }
+
+    static void menuOptions(){
+        System.out.println("Please select an option:");
+        System.out.println("\t#1-Show Hanoi Objects");
+        System.out.println("\t#2-Show Hanoi Disks");
+        System.out.println("\t#3-Show Hanoi Towers");
+        System.out.println("\t#4-Start Simulation");
+        System.out.println("\t#5-Resize Disks");
+        System.out.println("\t#6-Exit");
+        System.out.print("Your Choice: ");
+    }
+
 
     public static void main(String[] args){
+        boolean guiRun = true;
+        boolean subGui;
+        int[] menuOptionsArray = {1,2,3,4,5,6};
         HanoiTower one = new HanoiTower();      //from
         HanoiTower two = new HanoiTower();      //aux
         HanoiTower three = new HanoiTower();    //to
+        int simRun = 0;
 
-        HanoiSim simOne = new HanoiSim(2);
-        HanoiDisk[] disks= simOne.createDisks();
+        System.out.println("Welcome to the Tower's of Hanoi Simulation Program");
 
-        HanoiSim.initializeDiskSet(one, disks);
+        while(guiRun==true){
+            System.out.println("Please select the amount of disks:");
+            Scanner scan = new Scanner(System.in);
+            int diskAmt = scan.nextInt();
+            subGui = true;
 
-        simOne.towerSimulation(one.diskSet.size(), one, three, two);
+            while(subGui == true){
+                menuOptions();
+
+                int option = scan.nextInt();
+
+                if(option == menuOptionsArray[0]){
+                    //show hanoi objects
+
+
+                }else if(option == menuOptionsArray[1]){
+                    //show hanoi disks
+
+                }else if(option == menuOptionsArray[2]){
+                    //show hanoi towers
+
+                }else if(option == menuOptionsArray[3]){
+                    //start simulation
+                    HanoiSim simOne = new HanoiSim(diskAmt);
+                    HanoiDisk[] disks= simOne.createDisks();
+
+                    HanoiSim.initializeDiskSet(one, disks);
+                    simOne.towerSimulation(diskAmt, one, three, two);
+                    simOne.resetSimulation(one,two,three);
+
+                }else if(option == menuOptionsArray[4]){
+                    //resize disks
+
+                    subGui = false;
+                }else if(option == menuOptionsArray[5]){
+                    //exit
+
+                    subGui = false;
+                    guiRun = false;
+                }else{
+                    System.out.println("Sorry, please select a valid option");
+                }
+            }
+
+        }
+
+
+
+//        HanoiSim simOne = new HanoiSim(2);
+//        HanoiDisk[] disks= simOne.createDisks();
+//
+//        HanoiSim.initializeDiskSet(one, disks);
+//
+//        simOne.towerSimulation(one.diskSet.size(), one, three, two);
 
     }
 }
