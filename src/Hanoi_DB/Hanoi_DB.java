20fb89b173728c47dd0508f8f6beed57de26cf03
Karan Joshi <karanjoshi31@live.com>
2018-06-08 22:59:53 -0500

    finished insertion functions

--- src/Hanoi_DB/Hanoi_DB.java
+++ src/Hanoi_DB/Hanoi_DB.java
@@ -1,9 +1,8 @@
 package Hanoi_DB;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.sql.rowset.serial.SerialBlob;
+import javax.sql.rowset.serial.SerialClob;
+import java.sql.*;
 
 public class Hanoi_DB {
 
@@ -21,12 +20,12 @@ public class Hanoi_DB {
 
             String createSimulationTableSQL = "CREATE TABLE IF NOT EXISTS simulations(\n"
                     + " diskNumber integer PRIMARY KEY, \n"
-                    + " steps blob not null" + ");";
+                    + " steps text not null" + ");";
 
             String createSimTimerTableSQL = "CREATE TABLE IF NOT EXISTS simulationTimer(\n"
-                    + " id integer PRIMARY KEY, \n"
+                    + " id integer PRIMARY KEY AUTOINCREMENT, \n"
                     + " diskNumber integer not null, \n"
-                    + " time real not null" + ");";
+                    + " time integer not null" + ");";
 
             statement.execute(createSimulationTableSQL);
             statement.execute(createSimTimerTableSQL);
@@ -41,5 +40,58 @@ public class Hanoi_DB {
 
     public static void closeConnection() throws SQLException{
         conn.close();
+    }
+
+    public static void insertSteps(int diskNum, String steps) throws SQLException{
+        if(checkDisk(diskNum) == false){
+            //System.out.println("Disk Doesn't exist");
+            String insertRecord = ("insert into simulations(diskNumber, steps) values (?,?)");
+
+            try(PreparedStatement preparedStatement = conn.prepareStatement(insertRecord)){
+                preparedStatement.setInt(1, diskNum);
+
+                preparedStatement.setString(2, steps);
+
+                preparedStatement.executeUpdate();
+            }
+        }
+
+    }
+
+    public static void insertSimTim(int diskNum, long milliseconds) throws SQLException{
+
+//        Statement statement = conn.createStatement();
+//
+//        ResultSet resultSet =
+//                statement.executeQuery("select max (id) as \"max_ID\" from simulationTimer");
+//
+//        int newID = resultSet.getInt("max_ID")+1;
+
+        String insertRecord = ("insert into simulationTimer(diskNumber,time) values(?,?)");
+
+        try(PreparedStatement preparedStatement = conn.prepareStatement(insertRecord)){
+            preparedStatement.setInt(1,diskNum);
+            preparedStatement.setLong(2,milliseconds);
+            preparedStatement.executeUpdate();
+        }
+
+    }
+
+    private static boolean checkDisk(int diskNum) throws SQLException{
+
+        boolean exists = false;
+
+        Statement statement = conn.createStatement();
+        ResultSet diskResult
+                = statement.executeQuery("select count(*) as \"diskCount\" from simulations where diskNumber="
+                +String.valueOf(diskNum));
+
+        int resultSize = diskResult.getInt("diskCount");
+        System.out.println(resultSize);
+        if(resultSize>0){
+            exists = true;
+        }
+
+        return exists;
     }
 }
